a9fba6720fda05cac91d387ecdfd031fc122c4fd
tanujasaste <tanujasaste9.com>
2024-11-07 23:53:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var countdownDate = new Date("nov 9, 2024 00:00:00").getTime();
+var x = setInterval(function () {
+    var now = new Date().getTime();
+    var distance = countdownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.querySelector(".days").innerHTML = days;
+    document.querySelector(".hours").innerHTML = hours;
+    document.querySelector(".minutes").innerHTML = minutes;
+    document.querySelector(".seconds").innerHTML = seconds;
+}, 1000)
